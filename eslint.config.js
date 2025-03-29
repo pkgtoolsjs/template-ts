869aaf3762b216d501c50ff4444a139bb15a293e
hhui64 <907322015@qq.com>
2025-03-30 03:29:28 +0800

    chore(eslint): refactor ESLint configuration to enhance TypeScript support and ignore additional files

--- eslint.config.js
+++ eslint.config.js
@@ -4,29 +4,41 @@ import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import eslintConfigPrettier from 'eslint-config-prettier'
 
-export default tseslint.config({
-  files: ['**/*.ts'],
-  ignores: ['**/dist/**'],
-  extends: [
-    eslint.configs.recommended,
-    tseslint.configs.recommended,
-    eslintConfigPrettier
-  ],
-  plugins: {
-    '@typescript-eslint': tseslint.plugin
+export default tseslint.config(
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,tsx,mts,cts}']
   },
-  languageOptions: {
-    parser: tseslint.parser,
-    parserOptions: {
-      project: true,
-      sourceType: 'module',
-      ecmaVersion: 'latest'
+
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '*.config*']
+  },
+
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+
+  {
+    name: 'app/parser',
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname
+      }
     }
   },
-  rules: {
-    'no-unused-vars': 'warn',
-    '@typescript-eslint/no-unused-vars': 'warn',
-    'require-atomic-updates': 'off',
-    'no-async-promise-executor': 'off'
+  eslintConfigPrettier,
+
+  {
+    name: 'app/global-rules',
+    files: ['**/*.{ts,tsx,mts,cts}'],
+    rules: {
+      'no-undef': 'off',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'require-atomic-updates': 'off',
+      'no-async-promise-executor': 'off'
+    }
   }
-})
+)
