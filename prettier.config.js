ac922a94d15b21400e498f308dee5b2a1462a199
hhui64 <907322015@qq.com>
2025-04-02 01:21:48 +0800

    chore: migrate Prettier configuration to new format and update TypeScript configuration to extend @tsconfig/node22

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,8 @@
-module.exports = {
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import('prettier').Config}
+ */
+export default {
   semi: false,
   singleQuote: true,
   printWidth: 80,
